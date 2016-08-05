620c537aa7152832acf73870335c2c6e86ab4839
markus-hartung <markus.hartung@avira.com>
2016-08-05 17:05:45 +0100

    Made the example specific to changing settings

--- master/background.js
+++ master/background.js
@@ -1,16 +1,84 @@
 "use strict";
 
-const SLAVE_EXTENSION_ID = 'kdpefhhbeglahfpgjiffjdopccljgjkn';
+class EventEmitter {
+  constructor() {
+    this._listeners = [];
+  }
 
-function sum(a, b, callback) {
-  chrome.runtime.sendMessage(SLAVE_EXTENSION_ID, {a, b}, null, callback);
+  addListener(callback) {
+    this._listeners.push(callback);
+  }
+
+  trigger(...args) {
+    this._listeners.forEach((callback) => callback(...args));
+  }
 }
 
-setInterval(function() {
-  let a = Math.round(Math.random() * 5);
-  let b = Math.round(Math.random() * 5);
 
-  sum(a, b, function(result) {
-    console.log(a + ' + ' + b + ' = ' + result.sum);
-  });
+class RemoteSettings {
+  constructor(remoteExtensionId) {
+    this._remoteExtensionId = remoteExtensionId;
+    this.onChange = new EventEmitter();
+    chrome.runtime.onMessageExternal.addListener(this._onMessageExternal.bind(this));
+  }
+
+  set(key, value) {
+    return this._send({
+      command: 'settings:set',
+      key,
+      value
+    });
+  }
+
+  get(key) {
+    return this._send({
+      command: 'settings:get',
+      key
+    });
+  }
+
+  getAll() {
+    return this._send({
+      command: 'settings:getAll'
+    });
+  }
+
+  _send(message, callback) {
+    return new Promise((resolve, reject) => {
+      chrome.runtime.sendMessage(this._remoteExtensionId, message, null, function(response) {
+        if (typeof response === 'undefined') {
+          reject('Remote extension not present');
+        } else if (response.error) {
+          reject(response.error);
+        } else {
+          resolve(response.value);
+        }
+      });
+    });
+  }
+
+  _onMessageExternal(message, sender) {
+    if (sender.id === this._remoteExtensionId && message.command == 'settings:onChange') {
+      this.onChange.trigger(message.key, message.value, message.oldValue);
+    }
+  }
+}
+
+
+
+console.log('Master');
+
+const SLAVE_EXTENSION_ID = 'kdpefhhbeglahfpgjiffjdopccljgjkn';
+
+var remoteSettings = new RemoteSettings(SLAVE_EXTENSION_ID);
+
+remoteSettings.get('volume').then((volume) => console.log('Volume:', volume));
+remoteSettings.set('input', 'tape');
+remoteSettings.getAll().then((settings) => console.log('Settings:', settings));
+
+remoteSettings.onChange.addListener((key, newValue, oldValue) =>
+  console.log(key, 'was changed from', oldValue, 'to', newValue));
+
+setInterval(function() {
+  remoteSettings.set('volume', Math.round(Math.random() * 11));
 }, 5000);
--- slave/background.js
+++ slave/background.js
@@ -1,15 +1,110 @@
 "use strict";
 
-const MASTER_EXTENSION_ID = 'bajgjohkdiegndkpfccjohaipmghnegn'
-
-chrome.runtime.onMessageExternal.addListener(function(message, sender, response) {
-  console.log('Sender:', sender);
-  console.log('Message:', message);
-  // Let's make sure we only work for the master extension
-  if (sender.id !== MASTER_EXTENSION_ID) {
-    return;
+class EventEmitter {
+  constructor() {
+    this._listeners = [];
   }
 
-  var sum = message.a + message.b;
-  response({sum});
+  addListener(callback) {
+    this._listeners.push(callback);
+  }
+
+  trigger(...args) {
+    this._listeners.forEach((callback) => callback(...args));
+  }
+}
+
+
+class Settings {
+  constructor(initialSettings) {
+    this._settings = initialSettings;
+    this._remoteExtensions = {};
+    this.onChange = new EventEmitter();
+    chrome.runtime.onMessageExternal.addListener(this._onMessageExternal.bind(this));
+  }
+
+  set(key, value) {
+    let oldValue = this._settings[key];
+    if (value !== oldValue) {
+      this._settings[key] = value;
+      this.onChange.trigger(key, value, oldValue);
+      this._informRemotes(key, value, oldValue);
+    }
+  }
+
+  get(key) {
+    return this._send({
+      command: 'settings:get',
+      key
+    });
+  }
+
+  getAll() {
+    return this._settings;
+  }
+
+  addRemote(extensionId, allowedKeys) {
+    this._remoteExtensions[extensionId] = allowedKeys;
+  }
+
+  _informRemotes(key, value, oldValue) {
+    let message = {
+      command: 'settings:onChange',
+      key,
+      value,
+      oldValue
+    }
+    for (let remoteId in this._remoteExtensions) {
+      chrome.runtime.sendMessage(remoteId, message);
+    }
+  }
+
+  _onMessageExternal(message, sender, response) {
+    let remote = this._remoteExtensions[sender.id];
+    if (!remote) {
+      return;
+    }
+
+    let command = message.command;
+    let key = message.key;
+    switch (command) {
+      case 'settings:set':
+        if (remote.indexOf(key) != -1) {
+          this.set(key, message.value);
+          response({});
+        } else {
+          response({error: 'Remote is not allowed to control setting ' + key});
+        }
+        break;
+      case 'settings:get':
+        response({value: this._settings[key]});
+        break;
+      case 'settings:getAll':
+        response({value: this._settings});
+        break;
+    }
+  }
+}
+
+
+
+console.log('Slave');
+
+const MASTER_EXTENSION_ID = 'bajgjohkdiegndkpfccjohaipmghnegn';
+
+var settings = new Settings({
+  volume: 0,
+  bass: 8,
+  treble: 4,
+  input: 'cd'
 });
+settings.addRemote(MASTER_EXTENSION_ID, ['volume', 'input']);
+
+settings.onChange.addListener((key, newValue, oldValue) =>
+  console.log(key, 'was changed from', oldValue, 'to', newValue));
+
+settings.set('input', 'lp');
+
+setInterval(function() {
+  settings.set('treble', Math.round(Math.random() * 10));
+}, 5000);
